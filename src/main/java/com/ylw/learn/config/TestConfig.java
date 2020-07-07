fbb32681a9021807f9ea445a83f7d59cc610f841
343289546@qq.com <343289546@qq.com>
2020-07-07 16:15:34 +0800

    添加lombok 插件 、 添加日志

--- src/main/java/com/ylw/learn/config/TestConfig.java
+++ src/main/java/com/ylw/learn/config/TestConfig.java
@@ -1,19 +1,13 @@
 package com.ylw.learn.config;
 
+import lombok.Data;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "common.ylw")
+@Data
 public class TestConfig {
 	private String test;
-
-	public String getTest() {
-		return test;
-	}
-
-	public void setTest(String test) {
-		this.test = test;
-	}
-	
 }
--- src/main/java/com/ylw/learn/controller/TestController.java
+++ src/main/java/com/ylw/learn/controller/TestController.java
@@ -2,6 +2,8 @@ package com.ylw.learn.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ylw.learn.config.TestConfig;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
